e5f3ffb682a1091d4749039cb713f737eaed4133
Moxious <moxious@oldhat.org>
2019-02-04 16:21:26 -0500

    endless tweaking

--- benchmark.sh
+++ benchmark.sh
@@ -3,10 +3,11 @@
 TAG=$(head -c 3 /dev/urandom | md5 | head -c 5)
 LOG=log-benchmark-$TAG.log
 
-echo "====================================================" | tee >>$LOG
-echo "== BENCHMARK $TAG $1" | tee >>$LOG
-echo "== START: " $(date) | tee >>$LOG
-echo "====================================================" | tee >>$LOG
+echo "Benchmark $TAG starting, logging to $LOG"
+echo "====================================================" | tee $LOG
+echo "== BENCHMARK $TAG $1" | tee $LOG
+echo "== START: " $(date) | tee $LOG
+echo "====================================================" | tee $LOG
 
 if [ -z $2 ] ; then 
    echo "Usage: ./benchmark.sh bolt+routing://host:7687 Neo4jPassword"
@@ -24,28 +25,34 @@ export NEO4J_USERNAME=neo4j
 
 STARTTIME=$(date +%s)
 
-cat 01-index.cypher | cypher-shell -a $NEO4J_URI | tee >>$LOG
-./load-all.sh segment-files-subset.txt | tee >>$LOG
-cat 02b-load-world-cities.cypher | cypher-shell -a $NEO4J_URI | tee >>$LOG
-cat 03a-link-groups-to-countries.cypher | cypher-shell -a $NEO4J_URI | tee >>$LOG
-cat 03b-link-venues-to-cities.cypher | cypher-shell -a $NEO4J_URI | tee >>$LOG
+echo "Index phase" | tee $LOG
+cat 01-index.cypher | cypher-shell -a $NEO4J_URI >>$LOG
+echo "Load phase" | tee $LOG
+./load-all.sh segment-files-subset.txt >>$LOG
+echo "Cities phase" | tee $LOG
+cat 02b-load-world-cities.cypher | cypher-shell -a $NEO4J_URI >>$LOG
+echo "Link groups phase" | tee $LOG
+cat 03a-link-groups-to-countries.cypher | cypher-shell -a $NEO4J_URI >>$LOG
+echo "Link venues phase" | tee $LOG
+cat 03b-link-venues-to-cities.cypher | cypher-shell -a $NEO4J_URI >>$LOG
 
 for q in `seq 1 5` ; do 
-    rm -f queryload-$q.cypher
+    echo "Queryload $q phase" | tee $LOG
     for i in `seq 1 10` ; do 
         cat benchmark/q$q >> queryload-$TAG-$q.cypher
     done
 
-    echo "Running queryload $q..." | tee >>$LOG
-    cat queryload-$TAG-$q.cypher | cypher-shell -a $NEO4J_URI | tee >>$LOG
+    cat queryload-$TAG-$q.cypher | cypher-shell -a $NEO4J_URI >>$LOG
 done
 ENDTIME=$(date +%s)
-echo "BENCHMARK ELAPSED TIME IN SECONDS: " $(($ENDTIME - $STARTTIME))
+ELAPSED=$(($ENDTIME - $STARTTIME))
+echo "BENCHMARK ELAPSED TIME IN SECONDS: " $ELAPSED | tee $LOG
 
 rm -f queryload-$TAG-*.cypher
-echo "Done" | tee >> $LOG
+echo "Done" | tee $LOG
 
-echo "====================================================" | tee >>$LOG
+echo "====================================================" >>$LOG
 echo "== BENCHMARK $TAG $1" | tee >>$LOG
 echo "== FINISH: " $(date) | tee >>$LOG
-echo "====================================================" | tee >>$LOG
+echo "====================================================" >>$LOG
+echo "Benchmark $TAG complete with $ELAPSED elapsed; logging to $LOG" | tee $LOG
